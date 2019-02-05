b961fbaaad522b04448ce718982c7c155a8d926c
David Mejia <franklin.mejia@epn.edu.ec>
2019-02-05 13:00:17 -0500

    CreacionEntidadesRolUsuario

--- examen-web/src/usuario/usuario.entity.ts
+++ examen-web/src/usuario/usuario.entity.ts
@@ -1,9 +1,28 @@
 import {BeforeInsert, Column, Entity, Index, OneToMany, PrimaryColumn, PrimaryGeneratedColumn} from 'typeorm';
+import {RolEntity} from "../rol/rol.entity";
 // import {PaginaEntity} from "../pagina/pagina.entity";
 @Entity('usuario') // Es buena practica poner explicitamente el nombre de la tabla que queremos
 export class UsuarioEntity {
 
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  nombre: string;
+
+  @Column()
+  correo: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  fecha_nacimiento: string;
+
+  @OneToMany(
+      type => RolEntity,   //Que tabla vamos a relacionar
+      rol => rol.id //Campo que hace referencia como el Foreing key
+  )
+  roles: RolEntity[];
 
 }
