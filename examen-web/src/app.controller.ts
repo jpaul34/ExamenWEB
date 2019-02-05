7e47ad068cfe406ca14af680172f6a423de4f123
David Mejia <franklin.mejia@epn.edu.ec>
2019-02-05 12:07:36 -0500

    Inicio

--- examen-web/src/app.controller.ts
+++ examen-web/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Get, Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller()//localhost:3000
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+//router.get('/hello')
+//localhost:3000/hello
   @Get()
-  root(): string {
+  root(): any {
     return this.appService.root();
   }
 }
